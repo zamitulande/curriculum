04433df43f8e36aaa33543142a904da6abd77c5c
zami tulande <hulmartulande@gmail.com>
2022-02-15 18:00:28 -0500

    descarga

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Foto from '../image/foto.png'
+import Pdf from '../image/cv-actualizado.pdf'
 
 const Header = () => {
   return (
@@ -8,6 +9,7 @@ const Header = () => {
         <div class="nombre_rol">
           <div class="nombre">HULMAR ZAMI TULANDE ANTE</div>          
           <div class="rol">DESARROLLADOR FRONT-END</div>
+          <div class="descarga"> <a class="a_link" download={Pdf} href={Pdf}>descargar pdf</a></div>
         </div>
     </div>
   )
