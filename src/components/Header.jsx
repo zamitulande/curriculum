1dee56ad3089330a23e2b3c8116dc14b38369c2f
zami tulande <hulmartulande@gmail.com>
2022-02-16 09:05:09 -0500

    modificar descarga

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,10 @@ const Header = () => {
         <div class="nombre_rol">
           <div class="nombre">HULMAR ZAMI TULANDE ANTE</div>          
           <div class="rol">DESARROLLADOR FRONT-END</div>
-          <div class="descarga"> <a class="a_link" download={Pdf} href={Pdf}>descargar pdf</a></div>
+          <div class="descarga"> <a class="d_link" download={Pdf} href={Pdf}>
+          <i class="fa-solid fa-cloud-arrow-down"></i> 
+          <i class="fa-regular fa-file-pdf"></i>
+          </a></div>
         </div>
     </div>
   )
